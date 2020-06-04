c5222c956dbd1b3c8cc652aa022104893053a84b
Ming119 <57996391+Ming119@users.noreply.github.com>
2020-06-04 17:05:00 +0800

    Update 001.c

--- 001.c
+++ 001.c
@@ -1,5 +1,5 @@
-//	Created by L.H.Ming on 2020/03/10.
-//  Copyright © 2020 L.H.Ming. All rights reserved.
+// Created by L.H.Ming on 2020/03/10.
+// Copyright © 2020 L.H.Ming. All rights reserved.
 
 /*
 001. 一元二次方程式
@@ -41,36 +41,23 @@ Input
 Output
 2.0
 2.0
-
-*/
-
-/*
-	解法說明
-
-	1.	分別輸入a, b, c到int
-	2.	根據公式計算兩個實根
-	3.	輸出兩個實根
-	
 */
 
 #include <stdio.h>
 #include <math.h>
 
 int main() {
-	//初始化
-    int a, b, c;
+    int a, b, c;	//初始化
+    
+    scanf("%d%d%d", &a, &b, &c);	//輸入
 	
-	//輸入
-    scanf("%d%d%d", &a, &b, &c);
-	
-	//計算兩個實根
+    //計算兩個實根
     float determinant = b*b-4*a*c;
     float x1 = ((-b)+sqrt(determinant))/(2*a);
     float x2 = ((-b)-sqrt(determinant))/(2*a);
 
-	//輸出
-    printf("%.1f\n%.1f", x1, x2);
+    printf("%.1f\n%.1f", x1, x2);	//輸出
 	
-	return 0;
+    return 0;
 }
 
